fc6c2ff95c372ae69cf78c0d57b6633f19a40168
Peter <peterogb067@gmail.com>
2025-09-06 02:01:25 +0100

    Add UserProfile component with props example

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,17 +2,24 @@
 import './App.css';
 
 // Import the new components
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <>
-      <Header />
-      <MainContent />
-      <Footer />
-    </>
+     {/* Example User Profile */}
+     <UserProfile
+     name="Alice"
+     age="25"
+     bio="Loves hiking and photography"
+    />
+
+    <UserProfile 
+     name="John"
+     age="30"
+     bio="A passionate web developer and traveler"
+    />
+  </>
   );
 }
 
