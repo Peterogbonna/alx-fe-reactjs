415f8286b1d2cf1f863bdd989035b841864eea6d
Peter <peterogb067@gmail.com>
2025-09-28 00:33:41 +0100

    updated

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -1,9 +1,12 @@
 // Ensure you import the React plugin
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react' // <--- REQUIRED IMPORT
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   // Ensure the plugin is included in the plugins array
-  plugins: [react()], // <--- REQUIRED USAGE
-  // ... other config
+  plugins: [
+    react(),
+    tailwindcss(), // <--- REQUIRED USAGE
+  ], 
 })
