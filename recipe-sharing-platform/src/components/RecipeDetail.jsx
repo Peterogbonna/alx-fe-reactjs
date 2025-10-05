8afafd8076e3c7e579f4fa97f92eb2606b807776
Peter <peterogb067@gmail.com>
2025-10-05 20:26:23 +0100

    Add useState and UseEffect

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,5 +1,7 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useSearchParams } from "react-router-dom";
+import ( useState, useEffect ) from "react"
 import data from "../data.json";
+import { useEffect, useState } from "react";
 
 function RecipeDetail() {
   const { id } = useParams();
