d2724f1f708deea95a70214881efe02731384807
Peter <peterogb067@gmail.com>
2025-10-18 23:29:51 +0100

    New updated

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,43 +1,21 @@
 // src/App.jsx
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Profile from "./pages/Profile";
-import ProfileDetails from "./pages/ProfileDetails";
-import ProfileSettings from "./pages/ProfileSettings";
+import Profile from "./components/Profile";
 import BlogPost from "./pages/BlogPost";
-import Login from "./pages/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Basic routes */}
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/profile/*" element={<Profile />} />
 
-        {/* ✅ Protected route with nested routes */}
-        <Route
-          path="/profile/*"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        >
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route> {/* 👈 THIS closing tag was missing */}
-
-        {/* Dynamic routing */}
-        <Route path="/blog/:postId" element={<BlogPost />} />
-
-        {/* Login route */}
-        <Route path="/login" element={<Login />} />
-
-        {/* Redirect invalid paths */}
-        <Route path="*" element={<Navigate to="/" />} />
+        {/* ✅ Dynamic Route for Blog Posts */}
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
