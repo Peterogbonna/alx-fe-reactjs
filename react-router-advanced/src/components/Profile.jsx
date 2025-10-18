fea9f7b4ffe464b8e34f7229c3eaf7e051884da1
Peter <peterogb067@gmail.com>
2025-10-18 23:28:01 +0100

    Updated

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,16 +1,28 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Routes, Route, Link, Outlet } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 function Profile() {
   return (
-    <div>
-      <h2>User Profile</h2>
-      <nav>
-        <Link to="details">Profile Details</Link> |{" "}
+    <div style={{ padding: "20px" }}>
+      <h2>👤 Profile Page</h2>
+
+      {/* Navigation links for nested routes */}
+      <nav style={{ marginBottom: "15px" }}>
+        <Link to="details" style={{ marginRight: "10px" }}>
+          Profile Details
+        </Link>
         <Link to="settings">Profile Settings</Link>
       </nav>
 
-      {/* Nested content will show here */}
+      {/* Nested routes */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+
+      {/* Optional Outlet if used by parent routes */}
       <Outlet />
     </div>
   );
