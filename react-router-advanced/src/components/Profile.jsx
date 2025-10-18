e157d5da8163728d6ba942fa49b193a5818a0594
Peter <peterogb067@gmail.com>
2025-10-18 23:25:18 +0100

    Updated

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
+function Profile() {
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <nav>
+        <Link to="details">Profile Details</Link> |{" "}
+        <Link to="settings">Profile Settings</Link>
+      </nav>
+
+      {/* Nested content will show here */}
+      <Outlet />
+    </div>
+  );
+}
+
+export default Profile;
